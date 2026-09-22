a29f1307d6b4b0beb2b4e227b727532a8f77a0a0
Drworm82 <worm_jim@hotmail.com>
2026-09-21 21:26:50 -0600

    PoC-01: add Death Bag prototype

---
+++ source/server/world/components/data_request_handlers/death_bag.pickup.gd
@@ -0,0 +1,11 @@
+extends DataRequestHandler
+
+func data_request_handler(
+\tpeer_id: int,
+\tinstance: ServerInstance,
+\targs: Dictionary
+) -> Dictionary:
+\tvar bag_id: int = int(args.get("bag_id", 0))
+\tif WorldServer.curr == null or WorldServer.curr.death_bag_service == null:
+\t\treturn {"ok": false, "reason": "service_unavailable"}
+\treturn WorldServer.curr.death_bag_service.pickup(peer_id, instance, bag_id)

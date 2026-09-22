22543dcc53a8401417a909ed5b1102a3d242621d
Drworm82 <worm_jim@hotmail.com>
2026-09-21 22:46:48 -0600

    Add client main lifecycle diagnostics

--- source/client/client_main.gd
+++ source/client/client_main.gd
@@ -1,6 +1,10 @@
 extends Node
 
-
 func _ready() -> void:
+	print("[CLIENT] ClientMain _ready. GameMode=", GameMode.mode(), " is_client=", GameMode.is_client())
+	if not tree_exiting.is_connected(_on_tree_exiting):
+		tree_exiting.connect(_on_tree_exiting)
 	multiplayer.multiplayer_peer = null
-	#get_tree().set_multiplayer(null)
+
+func _on_tree_exiting() -> void:
+	print("[CLIENT] ClientMain is exiting the scene tree.")

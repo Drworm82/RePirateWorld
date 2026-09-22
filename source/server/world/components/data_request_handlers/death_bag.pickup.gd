2914a46a2c62daf92110f0b0bdc954317d058437
Drworm82 <worm_jim@hotmail.com>
2026-09-21 21:33:51 -0600

    refactor: access DeathBagService through instance manager

--- source/server/world/components/data_request_handlers/death_bag.pickup.gd
+++ source/server/world/components/data_request_handlers/death_bag.pickup.gd
@@ -6,6 +6,6 @@ func data_request_handler(
 \targs: Dictionary
 ) -> Dictionary:
 \tvar bag_id: int = int(args.get("bag_id", 0))
-\tif WorldServer.curr == null or WorldServer.curr.death_bag_service == null:
+\tif WorldServer.curr == null or WorldServer.curr.instance_manager.death_bag_service == null:
 \t\treturn {"ok": false, "reason": "service_unavailable"}
-\treturn WorldServer.curr.death_bag_service.pickup(peer_id, instance, bag_id)
+\treturn WorldServer.curr.instance_manager.death_bag_service.pickup(peer_id, instance, bag_id)

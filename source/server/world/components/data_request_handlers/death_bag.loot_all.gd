a9accf9c1a9cd036e1bedbf76053a675ed373393
Drworm82 <worm_jim@hotmail.com>
2026-09-22 15:01:48 -0600

    PoC-02: add Death Bag loot handlers

---
+++ source/server/world/components/data_request_handlers/death_bag.loot_all.gd
@@ -0,0 +1,11 @@
+extends DataRequestHandler
+
+func data_request_handler(
+    peer_id: int,
+    instance: ServerInstance,
+    args: Dictionary
+) -> Dictionary:
+    var bag_id: int = int(args.get("bag_id", 0))
+    if WorldServer.curr == null or WorldServer.curr.instance_manager.death_bag_service == null:
+        return {"ok": false, "reason": "service_unavailable"}
+    return WorldServer.curr.instance_manager.death_bag_service.loot_all(peer_id, instance, bag_id)

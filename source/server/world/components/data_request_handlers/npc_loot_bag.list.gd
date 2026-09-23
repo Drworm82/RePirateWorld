489249bea433138f6da4cfe5dc138ebb8b40ed2e
Drworm82 <worm_jim@hotmail.com>
2026-09-23 13:41:03 -0600

    feat: add temporary NPC loot bag service

---
+++ source/server/world/components/data_request_handlers/npc_loot_bag.list.gd
@@ -0,0 +1,9 @@
+extends DataRequestHandler
+
+func data_request_handler(peer_id: int, instance: ServerInstance, args: Dictionary) -> Dictionary:
+	if instance == null or WorldServer.curr == null:
+		return {"ok": false, "bags": []}
+	var service = WorldServer.curr.instance_manager.npc_loot_bag_service
+	if service == null:
+		return {"ok": false, "bags": []}
+	return {"ok": true, "bags": service.list_for_instance(str(instance.instance_resource.instance_name))}
